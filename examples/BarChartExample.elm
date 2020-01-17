7237d22bde1f5a73d1b6190b9007df8b4870b8af
Henne <henne90gen@gmail.com>
2020-01-17 17:23:47 +0100

    Finished BarChartExample

--- examples/BarChartExample.elm
+++ examples/BarChartExample.elm
@@ -1,12 +1,12 @@
 module BarChartExample exposing (main)
 
+import BarChart
 import Browser
 import DataFrame exposing (DataFrame)
 
 
 type alias Model =
-    { df : DataFrame DataPoint
-    }
+    DataFrame DataPoint
 
 
 type alias DataPoint =
@@ -27,3 +27,47 @@ main =
         , update = update
         , subscriptions = subscriptions
         }
+
+
+init : flags -> ( Model, Cmd Msg )
+init _ =
+    ( DataFrame.create exampleData, Cmd.none )
+
+
+exampleData : List DataPoint
+exampleData =
+    [ dataPoint 0 10
+    , dataPoint 1 15
+    , dataPoint 2 12
+    , dataPoint 3 9
+    , dataPoint 4 13
+    , dataPoint 5 17
+    , dataPoint 6 11
+    ]
+
+
+dataPoint : Int -> Float -> DataPoint
+dataPoint weekday value =
+    { weekday = weekday, value = value }
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update _ model =
+    ( model, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+view : Model -> Browser.Document Msg
+view model =
+    { title = "BarChart Example"
+    , body =
+        [ BarChart.singleBarChart ( 600, 300 )
+            (\e -> String.fromInt e.weekday)
+            (\e -> e.value)
+            model
+        ]
+    }
